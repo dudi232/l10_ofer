88967ac57168c540f0f2acc2248ecfb4146b9efa
dudi232 <135500609+dudi232@users.noreply.github.com>
2023-09-03 11:26:20 +0300

    Add files via upload

---
+++ db/mongoConnect.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/idf9');
+  // await mongoose.connect('mongodb+srv://koko9:MONKE@cluster0.jfgkq.mongodb.net/idf9');
+  console.log("mongo connect idf9 atlas");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
